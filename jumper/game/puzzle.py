d9433cee6cdb49756d4e6fcb7ee3d693f1deaf67
techncrypt <techncrypt@gmail.com>
2022-02-03 21:19:33 -0700

    Correction

--- jumper/game/puzzle.py
+++ jumper/game/puzzle.py
@@ -15,7 +15,7 @@
 #      _hide_word = ['_ ','_ ','_ ','_ ','_ ','_']
         """This will replace the current letter with a line until the user correctly guesses the letter"""
 
-#Bryan
+#Cooper
 #      _chosen_word = _words[random.randint[0, 11]]
         """This will randomly choose a word from the list using random"""
 
@@ -23,6 +23,6 @@
 #      _guess = ''
         """Setting a blank value for the guess"""
 
-#Erin
+#Bryan
 #     - Add method replace_blank()
         """Based on the input user it will check to see if the letter is in the word."""
