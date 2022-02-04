77cdfe1c5d4ad697c6ec05308b94c4e56baa8e9a
techncrypt <techncrypt@gmail.com>
2022-02-03 21:01:07 -0700

    More Comments

--- jumper/game/Director.py
+++ jumper/game/Director.py
@@ -15,7 +15,7 @@
         """"""
 
     # Define a attribute called "_is_playing"
-        """"""
+        """Use a while loop to check if the game has ended and if so, to ask the user to keep playing or end game."""
 
 
     # Define a start_game() with the below
